ef55909864cbfb9ad1c99f4e7a6efaa033e06eaa
Joshua-05 <danil_kuznecov3020@mail.ru>
2024-12-05 19:13:30 +0400

    add cors in server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:5173']
+    }
+  });
   await app.listen(3000);
 }
 bootstrap();
