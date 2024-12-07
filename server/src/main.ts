96d1016ff2ca2557a29f3fe4d8fa8f1bc5846196
Joshua-05 <danil_kuznecov3020@mail.ru>
2024-12-07 17:40:57 +0400

    connect to data base

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: ['http://localhost:5173']
@@ -9,6 +10,6 @@ async function bootstrap() {
   });
 
   
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
--- server/src/modules/app/app.module.ts
+++ server/src/modules/app/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'hj;ltcndj8',
-      database: 'ListCRM',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       models: [],
       autoLoadModels: true,
     }),
